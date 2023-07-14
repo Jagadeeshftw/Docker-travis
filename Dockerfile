7e1c665aaee9294eee281d17d11f08f99571c7e3
jagadeeshftw <jagadeesh26062002@gmail.com>
2023-07-15 00:10:49 +0530

    production build successful

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
-FROM node:alpine
+FROM node:alpine as builder
 WORKDIR '/app' 
 COPY package.json .
 RUN npm install 
 COPY . .
-CMD ["npm","start"]
+CMD ["npm","run","build"]
+
+FROM nginx
+COPY --from=builder /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello boys!</p>
+        <p>Hello boys and  girls!</p>
       </header>
     </div>
   );
